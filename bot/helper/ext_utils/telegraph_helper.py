9de02d00d386e1873a4e7279eacddac582888576
KOT DEVELOPERS <95179243+KOT-BOTS-REPO@users.noreply.github.com>
2021-12-05 21:20:09 +0530

    Update telegraph_helper.py

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -57,4 +57,4 @@ class TelegraphHelper:
 			return self.edit_page(path, title, content)
 
 
-telegraph=TelegraphHelper('🎯𝗠𝗦𝗣 𝗕𝗼𝘁𝘀', 'https://t.me/MSPbots')
+telegraph=TelegraphHelper('KOT BOTS', 'https://t.me/KOT_')
